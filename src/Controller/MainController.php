38e3abbf5280870d96e12dd5ae5ca70dc3437c24
dcloarec2023 <david.cloarec2023@campus-eni.fr>
2023-06-15 11:26:43 +0200

    last commit

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +21,22 @@ class MainController extends AbstractController
     #[Route('/about-us', name:'about')]
     public function about() : Response
     {
+
+
         return $this->render('main/about.html.twig');
     }
+
+    #[Route('/road-to-dql', name: 'roadToDQL')]
+    public function roadToDQL(AnimalRepository $repo){
+//        $animal = new Animal();
+//        $animal->setName('toto');
+//        $entityManager->persist($animal);
+//        $entityManager->flush();
+//        $animals = $repo->findAll();
+//        $animal = $repo->findOneBy(['name' => 'toto']);
+//        $animals = $repo->findByName('toto');
+
+        return $this->render('main/road.html.twig');
+    }
+
 }
--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,34 +2,62 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\DocBlock\Tags\Formatter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('/wish', name: 'wish_')]
 class WishController extends AbstractController
 {
-    private $wishes = [
-        1 => "Faire du Java",
-        2 => "Rencontrer Chuck norris",
-        3 => "Arreter de rever de Java",
-        4 => "Faire un kahoot"
-    ];
+//    private $wishes = [
+//        1 => "Faire du Java",
+//        2 => "Rencontrer Chuck norris",
+//        3 => "Arreter de rever de Java",
+//        4 => "Faire un kahoot"
+//    ];
 
 
     #[Route('/list', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
+        $wishes = $wishRepository->findBy([],['dateCreated'=>'DESC']);
         return $this->render('wish/list.html.twig',[
-            'wishes' => $this->wishes
+            'wishes' => $wishes
         ]);
     }
 
     #[Route('/detail/{id}', name: 'detail', requirements: ['id' => '\d+'])]
-    public function detail($id): Response
+    public function detail($id, WishRepository $wishRepository): Response
     {
-        $wish = $this->wishes[$id];
+
+        $wish = $wishRepository->find($id);
         return $this->render('wish/detail.html.twig',[
             'wish' => $wish
+        ]);
+    }
+
+    #[Route('/ajouter', name: 'add', methods: 'GET')]
+    public function add(): Response
+    {
+        return $this->render('wish/add.html.twig');
+    }
+
+    #[Route('/ajouter', name: 'addPost', methods: 'POST')]
+    public function addPost(EntityManagerInterface $entityManager): Response
+    {
+        $wish = new Wish();
+        $wish->setTitle($_POST['title']);
+        $wish->setDescription($_POST['description']);
+        $wish->setAuthor($_POST['author']);
+        $wish->setIsPublished(true);
+        $wish->setDateCreated(new \DateTime());
+        $entityManager->persist($wish);
+        $entityManager->flush();
+        return $this->render('wish/add.html.twig',[
+            'ajout'=>'Ajouté'
         ]);
     }
 }
